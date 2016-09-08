ae55c68e36e8ed718cb5cfe87f7cc0621dc82735
thuynt <thuynt@bit-vietnam.com>
2016-09-08 14:19:05 +0700

    Edit home page

--- home.php
+++ home.php
@@ -3,5 +3,23 @@
 echo "This is home page";
 // assign value
 $title = "Welcome to this website";
+# get menu
+$menu = "home_menu";
+
+function getMenuIndex($name){
+	if ($name = "home_menu"){
+		return 1;
+	}
+	return 2;
+}
+
+function getTotal($a, $b){
+	if ($a > 0 && $b > 0){
+		return $a + $b;
+		
+	}
+	
+	return 0;
+}
 
 ?>
